078ab87129c66a14f2b13f3892510d620c9cc8bf
psalaza <43583947+psalaza@users.noreply.github.com>
2021-05-12 16:26:06 -0400

    Test for empty string and added all Roman Numeral options

--- RomanNumerals/RomanNumeralsApp/RomanNumeral.cs
+++ RomanNumerals/RomanNumeralsApp/RomanNumeral.cs
@@ -4,6 +4,7 @@ namespace RomanNumeralsApp
 {
     public static class RomanNumeral
     {
+
         public static int ConvertRomanNumeral(string romanNumeral)
         {
             if (romanNumeral is null)
@@ -14,8 +15,36 @@ namespace RomanNumeralsApp
             {
                 return 1;
             }
+            else if (romanNumeral == "V")
+            {
+                return 5;
+            }
+            else if (romanNumeral == "X")
+            {
+                return 10;
+            }
+            else if (romanNumeral == "L")
+            {
+                return 50;
+            }
+            else if (romanNumeral == "C")
+            {
+                return 100;
+            }
+            else if (romanNumeral == "D")
+            {
+                return 500;
+            }
+            else if (romanNumeral == "M")
+            {
+                return 1000;
+            }
+            else if( romanNumeral == ""){
+                return 0;
+            }
+            else    
+                throw new System.Exception();
 
-            return 0;
         }
 
         public static void Main()
--- RomanNumerals/RomanNumeralsTests/ConvertRomanNumeral_Tests.cs
+++ RomanNumerals/RomanNumeralsTests/ConvertRomanNumeral_Tests.cs
@@ -9,7 +9,13 @@ namespace RomanNumeralsTests
     {
         [Theory]
         [InlineData("I",1)]
-        // [InlineData("V",5)]
+        [InlineData("V",5)]
+        [InlineData("X",10)]
+        [InlineData("L",50)]
+        [InlineData("C",100)]
+        [InlineData("D",500)]
+        [InlineData("M",1000)]
+
         public void Should_Return1IfRomanNumeralIIsPassed(string romanNumeral, int value)
         {
             int number = RomanNumeral.ConvertRomanNumeral(romanNumeral);
@@ -23,6 +29,14 @@ namespace RomanNumeralsTests
             Action action = () => RomanNumeral.ConvertRomanNumeral(null);
 
             action.Should().Throw<NullReferenceException>();
+        }
+
+            [Fact]
+        public void Should_ValidatedIfStringPassedIsEmpty()
+        {
+
+            int number = RomanNumeral.ConvertRomanNumeral("");
+            number.Should().Be(0);
         }
     }
 }
