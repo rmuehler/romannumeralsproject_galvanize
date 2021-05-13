a32df2c155b16950cfa1be73d62497a7b01e5acd
Robert Muehler <robertmuehler@gmail.com>
2021-05-13 09:45:14 -0400

    refactored the RomanNumeral class

--- RomanNumerals/RomanNumeralsApp/RomanNumeral.cs
+++ RomanNumerals/RomanNumeralsApp/RomanNumeral.cs
@@ -4,49 +4,51 @@ namespace RomanNumeralsApp
 {
     public static class RomanNumeral
     {
-        public static int ConvertRomanNumeral(string romanNumeral)
+        public static int ConvertRomanNumeral(char romanNumeral)
         {
-            if (romanNumeral is null)
+            switch (romanNumeral)
             {
-                throw new NullReferenceException("Enter a non-null roman numeral");
+                // case : return 0;
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: throw new NullReferenceException("Enter a correct Roman Numeral character");
             }
-            if (romanNumeral.Length >= 2)
-            {
-                switch (romanNumeral[..2])
-                {
-                    case "IV":
-                        return 4;
-                    case "IX":
-                        return 9;
-                    case "XL":
-                        return 40;
-                    case "XC":
-                        return 90;
-                    case "CD":
-                        return 400;
-                    case "CM":
-                        return 900;
-                }
-            }
-            if (romanNumeral.Length >= 1)
-            {
-                return romanNumeral[..1] switch
-                {
-                    "I" => 1,
-                    "V" => 5,
-                    "X" => 10,
-                    "L" => 50,
-                    "C" => 100,
-                    "D" => 500,
-                    "M" => 1000,
-                };
-            }
-            Console.Out.WriteLine("Return 0");
+
             return 0;
         }
 
         public static void Main()
         {
+        }
+
+        public static int Convert(string romanNumeral)
+        {
+            
+            int answer = 0;
+
+            for (int i = 0; i < romanNumeral.Length; i++)
+            {
+                
+                int num1 = ConvertRomanNumeral(romanNumeral[i]);
+                
+
+                if (i + 1 < romanNumeral.Length)
+                {
+                    int num2 = ConvertRomanNumeral(romanNumeral[i + 1]);
+                }
+                else
+                {
+                    answer += num1;
+                }
+
+            }
+
+            return answer;
         }
     }
 }
