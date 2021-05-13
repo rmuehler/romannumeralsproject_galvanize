f1294ba9b9c14ce544ea6d76ec023142b1afcd8f
Robert Muehler <robertmuehler@gmail.com>
2021-05-13 09:32:57 -0400

    added new test for Convert function

--- RomanNumerals/RomanNumeralsTests/ConvertRomanNumeral_Tests.cs
+++ RomanNumerals/RomanNumeralsTests/ConvertRomanNumeral_Tests.cs
@@ -8,14 +8,14 @@ namespace RomanNumeralsTests
     public class UnitTest1
     {
         [Theory]
-        [InlineData("I", 1)]
-        [InlineData("V", 5)]
-        [InlineData("X", 10)]
-        [InlineData("L", 50)]
-        [InlineData("C", 100)]
-        [InlineData("D", 500)]
-        [InlineData("M", 1000)]
-        public void Should_Return1IfRomanNumeralIIsPassed(string romanNumeral, int value)
+        [InlineData('I', 1)]
+        [InlineData('V', 5)]
+        [InlineData('X', 10)]
+        [InlineData('L', 50)]
+        [InlineData('C', 100)]
+        [InlineData('D', 500)]
+        [InlineData('M', 1000)]
+        public void Should_Return1IfRomanNumeralIIsPassed(char romanNumeral, int value)
         {
             int number = RomanNumeral.ConvertRomanNumeral(romanNumeral);
 
@@ -25,38 +25,38 @@ namespace RomanNumeralsTests
         [Fact]
         public void Should_ThrowAnExceptionIfNullIsPassed()
         {
-            Action action = () => RomanNumeral.ConvertRomanNumeral(null);
-
+            Action action = () => RomanNumeral.ConvertRomanNumeral('\0');
+        
             action.Should().Throw<NullReferenceException>();
         }
 
-        [Fact]
-        public void Should_ValidatedIfStringPassedIsEmpty()
-        {
-            int number = RomanNumeral.ConvertRomanNumeral("");
-            number.Should().Be(0);
-        }
+        // [Fact]
+        // public void Should_ValidatedIfStringPassedIsEmpty()
+        // {
+        //     int number = RomanNumeral.ConvertRomanNumeral('\0');
+        //     number.Should().Be(0);
+        // }
 
-        [Theory]
-        [InlineData("IV", 4)]
-        [InlineData("IX", 9)]
-        [InlineData("XL", 40)]
-        [InlineData("XC", 90)]
-        [InlineData("CD", 400)]
-        [InlineData("CM", 900)]
-        public void Should_ReturnValueIfMultipleCharacterRomanNumeralPassed(string romanNumeral, int value)
-        {
-            int number = RomanNumeral.ConvertRomanNumeral(romanNumeral);
-
-            number.Should().Be(value);
-        }
-
-        [Fact]
-        public void Should_ReturnValueIfRomanNumeral3_OrMoreCharacter()
-        {
-            int number = RomanNumeral.ConvertRomanNumeral("MMVI");
-
-            number.Should().Be(2006);
-        }
+        // [Theory]
+        // [InlineData("IV", 4)]
+        // [InlineData("IX", 9)]
+        // [InlineData("XL", 40)]
+        // [InlineData("XC", 90)]
+        // [InlineData("CD", 400)]
+        // [InlineData("CM", 900)]
+        // public void Should_ReturnValueIfMultipleCharacterRomanNumeralPassed(string romanNumeral, int value)
+        // {
+        //     int number = RomanNumeral.ConvertRomanNumeral(romanNumeral);
+        //
+        //     number.Should().Be(value);
+        // }
+        //
+        // [Fact]
+        // public void Should_ReturnValueIfRomanNumeral3_OrMoreCharacter()
+        // {
+        //     int number = RomanNumeral.ConvertRomanNumeral("MMVI");
+        //
+        //     number.Should().Be(2006);
+        // }
     }
 }
---
+++ RomanNumerals/RomanNumeralsTests/NewTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using RomanNumeralsApp;
+using Xunit;
+
+namespace RomanNumeralsTests
+{
+    public class NewTest
+    {
+        // [InlineData("IV", 4)]
+        // [InlineData("IX", 9)]
+        // [InlineData("XL", 40)]
+        // [InlineData("XC", 90)]
+        // [InlineData("CD", 400)]
+        // [InlineData("CM", 900)]
+        // public void Should_ReturnValueIfMultipleCharacterRomanNumeralPassed(string romanNumeral, int value)
+        // {
+        //     int number = RomanNumeral.(romanNumeral);
+        //
+        //     number.Should().Be(value);
+        // }
+
+        [Fact]
+        public void Test()
+        {
+            int number = RomanNumeral.Convert("IV");
+
+            number.Should().Be(4);
+
+
+        }
+        
+    }
+}
